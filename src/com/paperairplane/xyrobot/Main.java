f1ecfa5e2255e2e39a60693d62ba50f6d05a9573
fython <fython@163.com>
2013-03-09 06:32:25 +0000

    加入了关于对话框 以及部分回答

--- src/com/paperairplane/xyrobot/Main.java
+++ src/com/paperairplane/xyrobot/Main.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -14,7 +18,9 @@ import android.widget.ListView;
 
 public class Main extends Activity {
 
+	static int i=0;
 	String UserMsg;
+	static String AI_UnknowMsg;
 	List<String> data = new ArrayList<String>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,11 @@ public class Main extends Activity {
 				EditText editText1 = (EditText) findViewById(R.id.editText1);
 				UserMsg = editText1.getText().toString();
 				editText1.setText("");
-				addItem(getString(R.string.myname) +":"+ UserMsg);
-				addItem(getString(R.string.robotname) +":"+ RobotAI.getAnswer(UserMsg));
+				addItem(getString(R.string.myname) +" : "+ UserMsg);
+				addItem(getString(R.string.robotname) +" : "+ RobotAI.getAnswer(UserMsg));
 			}
 		});
-		addItem(getString(R.string.robotname) +":"+ getString(R.string.AI_hello));
+		addItem(getString(R.string.robotname) +" : "+ getString(R.string.AI_hello));
 	}
 
 	public void addItem(String string){
@@ -42,6 +48,39 @@ public class Main extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem menu) {
+		super.onOptionsItemSelected(menu);
+		switch (menu.getItemId()) {
+		case R.id.menu_settings:
+			showAbout();
+			break;
+		}
+		return true;
+	}
+	
+	public Dialog onCreateDialog(final int _id) {
+		if (_id == R.layout.about) {
+			View about = LayoutInflater.from(this)
+					.inflate(R.layout.about, null);
+			Button button_about = (Button) about
+					.findViewById(R.id.button_about);
+			button_about.setOnClickListener(new OnClickListener() {
+				@SuppressWarnings("deprecation")
+				public void onClick(View v) {
+					removeDialog(R.layout.about);
+				}
+			});
+			return new AlertDialog.Builder(this).setView(about).create();
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void showAbout(){
+		showDialog(R.layout.about);
 	}
 
 }
--- src/com/paperairplane/xyrobot/RobotAI.java
+++ src/com/paperairplane/xyrobot/RobotAI.java
@@ -4,71 +4,159 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 public class RobotAI {
 
+	static int GoodMorning_events = 0;
 	private static int FindStr(String str0,String str1){
 		return str0.indexOf(str1);
 	}
 	
-	// 中考倒计时
-	public static String getZKDJS() throws ParseException{
-		 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		 Date b= sdf.parse("2013-6-19");
-		 Date a= Calendar.getInstance().getTime();
-		 long time  = (b.getTime()-a.getTime())/1000/60/60/24;
-		 return Long.toString(time);
-	}
-	// 高考倒计时
-	public static String getGKDJS() throws ParseException{
-		 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		 Date b= sdf.parse("2013-6-7");
-		 Date a= Calendar.getInstance().getTime();
-		 long time  = (b.getTime()-a.getTime())/1000/60/60/24;
-		 return Long.toString(time);
+	// 倒计时
+	private static String getDJS(String date) throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date thatday = sdf.parse(date);
+		Date nowtime = Calendar.getInstance().getTime();
+		long time  = (thatday.getTime()-nowtime.getTime())/1000/60/60/24;
+		return Long.toString(time);
 	}
 	
+	@SuppressWarnings("deprecation")
 	public static String getAnswer(String question){
 		if (FindStr(question.toLowerCase(),"hello") != -1){
 			return Math.round(Math.random()) != 0 ? "Hey guys!" : "Hello!";
 		}
+		if ((FindStr(question,"早晨") != -1)|(FindStr(question,"早上") != -1 & FindStr(question,"好") != -1)|(FindStr(question,"早安") != -1)){
+			if (GoodMorning_events != 0){
+				return "你刚刚不是说了吗!?";
+			}
+			if (Calendar.getInstance().getTime().getHours() >= 0 & Calendar.getInstance().getTime().getHours() < 6){
+				return "早啊!这么早就醒来了?";
+			}
+			if (Calendar.getInstance().getTime().getHours() >= 6 & Calendar.getInstance().getTime().getHours() < 9){
+				return "早上好!";
+			}
+			if (Calendar.getInstance().getTime().getHours() >= 9 & Calendar.getInstance().getTime().getHours() < 11){
+				GoodMorning_events = 1;
+				return "再不快点就迟到了!";
+			}
+			if (Calendar.getInstance().getTime().getHours() >= 11 & Calendar.getInstance().getTime().getHours() < 16){
+				GoodMorning_events = 2;
+				return "你是猪啊?睡到这么晚.";
+			}
+			if (Calendar.getInstance().getTime().getHours() >= 16){
+				return "还早上好啊！？这都下午了……";
+			}
+		} 
+		if ((FindStr(question,"周末") != -1)|(FindStr(question,"星期六") != -1)|(FindStr(question,"星期天") != -1)|(FindStr(question,"星期日") != -1)|(FindStr(question,"周日") != -1)|(FindStr(question,"周六") != -1)){
+			if (GoodMorning_events == 1){
+				GoodMorning_events = -1;
+				return "噢，不用上学。";
+			}
+			if (GoodMorning_events == 2){
+				GoodMorning_events = -1;
+				return "那慢慢睡吧，不打扰你了。";
+			}
+			return "嗯!真好.";
+		}
+		if ((FindStr(question,"放假") != -1)|(FindStr(question,"节日") != -1)|(FindStr(question,"国庆") != -1)|(FindStr(question,"五一") != -1)|(FindStr(question,"过年") != -1)|(FindStr(question,"寒假") != -1)|(FindStr(question,"暑假") != -1)){
+			if (GoodMorning_events == 1){
+				GoodMorning_events = -1;
+				return "噢，不用上学。";
+			}
+			if (GoodMorning_events == 2){
+				GoodMorning_events = -1;
+				return "起来动一动吧别老窝在被窝里头。";
+			}
+			return "嘿嘿,多出去玩玩吧。";
+		}
+		if ((FindStr(question,"烧饼") != -1)|(FindStr(question,"fython") != -1)){
+			return Math.round(Math.random()) != 0 ? "他是个帅哥，哈哈哈哈哈哈哈哈。(纯属无聊非自恋啊)" : "他很傻的，一般人我不说出来...(嘘！别传出去)";
+		}
 		if ((FindStr(question,"你好") != -1)|(FindStr(question,"嗨") != -1)){
 			return Math.round(Math.random()) != 0 ? "嗨！" : "你好！";
 		}
+		if ((FindStr(question,"你好") != -1)|(FindStr(question,"嗨") != -1)){
+			return Math.round(Math.random()) != 0 ? "嗨！" : "你好！";
+		}
+		if ((FindStr(question,"在干") != -1)|(FindStr(question,"做什么") != -1)){
+			return Math.round(Math.random()) != 0 ? "陪你聊天呗。" : "在跟一个类人猿聊天";
+		}
+		if ((FindStr(question,"哈哈") != -1)|(FindStr(question,"嘻嘻") != -1)|(FindStr(question,"嘿嘿") != -1)){
+			if (GoodMorning_events == 1){
+				GoodMorning_events = -1;
+				return "还笑快去把。";
+			}
+			if (GoodMorning_events == 2){
+				GoodMorning_events = -1;
+				return "这么晚还笑得出来....";
+			}
+			return Math.round(Math.random()) != 0 ? "哈哈哈哈哈哈哈哈,在笑什么?" : "有什么好笑的?";
+		}
+		if (FindStr(question,"呵呵") != -1){
+			return "呵呵..";
+		}
 		if (FindStr(question.toLowerCase(),"time") != -1){
 			Date date = Calendar.getInstance().getTime();
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
 			return "It's "+formatter.format(date);
+		}
+		if (FindStr(question.toLowerCase(),"date") != -1){
+			Date date = Calendar.getInstance().getTime();
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+			return "It's "+formatter.format(date);
+		}
+		if (FindStr(question,"星期") != -1){
+			Date date = Calendar.getInstance().getTime();
+			SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+			String temp = formatter.format(date);
+			if ((FindStr(temp,"六") != -1)|(FindStr(temp,"日") != -1)|(FindStr(temp,"天") != -1)){
+				return "今天"+temp+"!太好了今天是周末。";
+			}
+			return "今天"+temp+"!";
 		}
 		if ((FindStr(question,"几点") != -1)|(FindStr(question,"时间") != -1)){
 			Date date = Calendar.getInstance().getTime();
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
 			return "现在的时间是 "+formatter.format(date);
 		}
 		if ((FindStr(question,"几号") != -1)|(FindStr(question,"日期") != -1)|(FindStr(question,"今天多少") != -1)){
 			Date date = Calendar.getInstance().getTime();
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			return "现在的时间是 "+formatter.format(date);
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+			String temp = new SimpleDateFormat("EEEE").format(date);
+			if ((FindStr(temp,"六") != -1)|(FindStr(temp,"日") != -1)|(FindStr(temp,"天") != -1)){
+				return "今天是 "+formatter.format(date)+"!今天是周末耶。";
+			}
+			return "今天是 "+formatter.format(date);
 		}
-		if ((FindStr(question,"中考") != -1)){
+		if (FindStr(question,"中考") != -1){
 			try {
-				return "离中考还有 "+getZKDJS()+" 天!加油!";
+				return "离中考还有 "+getDJS("2013-6-17")+" 天!加油!";
 			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				return "啊啊啊啊啊啊！程序错误！！";
+				return "";
 			}
 		}
 
-		if ((FindStr(question,"高考") != -1)){
+		if (FindStr(question,"高考") != -1){
 			try {
-				return "离高考还有 "+getZKDJS()+" 天!加油!";
+				return "离高考还有 "+getDJS("2013-6-7")+" 天!加油!";
 			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				return "啊啊啊啊啊啊！程序错误！！";
+				return "";
 			}
 		}
-		return Locale.getDefault() != Locale.CHINA ? "Sorry, I don't know what you mean." : "对不起,我不理解你的意思.";
+		if ((FindStr(question,"我去年买了个表") != -1)|(FindStr(question,"逼") != -1)|(FindStr(question,"草") != -1)|(FindStr(question,"冚") != -1)|(FindStr(question,"叼") != -1)){
+			Main.i++;
+			if (Main.i == 2){
+				return "你再骂我试试?";
+			}
+			if (Main.i == 3){
+				int a;
+				a = 1/0;
+				return Integer.toString(a);
+			}
+			return Math.round(Math.random()) != 0 ? "你妹" : "我去年买了个表";
+		}
+		return Main.AI_UnknowMsg;
 	}
 
 }
